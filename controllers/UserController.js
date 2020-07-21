d4c3be42d3d8a0fb9ddefff4a2a7913f351262bb
TimothyMoy <Timothy.z.moy@gmail.com>
2020-07-21 14:54:17 -0700

    added body-parser to server

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/User');
 
-// Current Path /User
+// Current Path /Admin
 
 // Index Route
 router.get('/', (req,res) => {
