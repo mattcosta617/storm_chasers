c458b80241284dc84b214258b6a9c3f6c9b184eb
mattcosta617 <mattiecosta@gmail.com>
2020-07-21 18:49:58 -0400

    push up to pull down

--- models/User.js
+++ models/User.js
@@ -8,20 +8,10 @@ const userSchema = new mongoose.Schema ({
         minlength: 1,
         maxlength: 40,
     }, 
-    lastName: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 40,
-    },
     email: {
         type: String,
         required: true,
         minlength: 3,
-    }, city: {
-        type: String,
-    }, state: {
-        type: String,
     },
 });
 
