42b4b656a689b20d1437df904388d601b09ece83
mattcosta617 <mattiecosta@gmail.com>
2020-07-21 20:20:30 -0400

    Users posting but not posting names and email

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,11 +6,31 @@ const db = require('../models/');
 
 // Index Route
 router.get('/', (req,res) => {
-  res.render('index');
-})
+  db.User.find({}, (err, allUsers) => {
+    if(err) return console.log(err);
+
+    res.render('user/index', {
+      users: allUsers,
+    });
+
+  })
+});
 
 
-router.post('/', (req, res) => {
+
+router.post('/', function(req, res){
+  db.User.create(req.body, (err, newUser) => {
+      db.User.findByIdAndUpdate(req.params.id, {
+          $push: {users: newUser}
+      }, (err, updatedUser) => {
+      
+          res.redirect(`/`);
+      })
+  });
+});
+
+
+router.post('/users', (req, res) => {
   console.log('Request Body = ', req.body)
   
   db.User.create(req.body, (err, newUser) => {
@@ -18,7 +38,7 @@ router.post('/', (req, res) => {
           db.User.findById(req.params._id, (err, foundUser) => {
               foundUser.users.push(newUser._id);
               foundUser.save((err, savedUser) => {
-              console.log('savedUser: ', savedUser);
+              console.log('savedUser: ', savedUser),
 
               res.redirect('/');
           });
